92fec9937d2e90ad3ef6f66554c6e0bb063bec0e
Storu Fujimori <satoru.fujimori@gmail.com>
2018-02-14 00:36:39 +0900

    Update indent

--- XFPrismSample/XFPrismSample/ViewModels/MainPageViewModel.cs
+++ XFPrismSample/XFPrismSample/ViewModels/MainPageViewModel.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Reactive.Bindings;
-
 using System.Reactive.Linq;
 
 namespace XFPrismSample.ViewModels
@@ -14,19 +13,17 @@ namespace XFPrismSample.ViewModels
     public class MainPageViewModel : ViewModelBase
     {
         public ReactiveCommand NextPageCommand { get; private set; }
-        public ReactiveProperty<string> Password { get;private set; }=new ReactiveProperty<string>("");
+        public ReactiveProperty<string> Password { get; private set; } = new ReactiveProperty<string>("");
 
-        public MainPageViewModel(INavigationService navigationService) 
-            : base (navigationService)
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
         {
             Title = "Main Page";
 
             this.NextPageCommand = this.Password
-                .Select(x => x== $"arrows") // 暗号が合っていれば
+                .Select(x => x == $"arrows") // 暗号が合っていれば
                 .ToReactiveCommand(); // 実行可能なCommandを作る
             this.NextPageCommand.Subscribe(async _ => await NavigationService.NavigateAsync("SecondPage"));
         }
-
-
     }
 }
