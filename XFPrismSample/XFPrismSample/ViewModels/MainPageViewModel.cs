199f15843bfaf4389a3888cb9590b167ef1a00f0
Storu Fujimori <satoru.fujimori@gmail.com>
2018-02-14 00:36:09 +0900

    暗号を入力したときだけボタンを押せるように修正

--- XFPrismSample/XFPrismSample/ViewModels/MainPageViewModel.cs
+++ XFPrismSample/XFPrismSample/ViewModels/MainPageViewModel.cs
@@ -7,20 +7,23 @@ using System.Linq;
 using System.Text;
 using Reactive.Bindings;
 
+using System.Reactive.Linq;
+
 namespace XFPrismSample.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        public ReactiveCommand NextPageCommand { get; private set; }=new ReactiveCommand();
+        public ReactiveCommand NextPageCommand { get; private set; }
+        public ReactiveProperty<string> Password { get;private set; }=new ReactiveProperty<string>("");
 
         public MainPageViewModel(INavigationService navigationService) 
             : base (navigationService)
         {
             Title = "Main Page";
 
-            //this.ClearCommand = this.Input
-            //    .Select(x => !string.IsNullOrWhiteSpace(x)) // Input.Valueが空じゃないとき
-            //    .ToReactiveCommand(); // 実行可能なCommandを作る
+            this.NextPageCommand = this.Password
+                .Select(x => x== $"arrows") // 暗号が合っていれば
+                .ToReactiveCommand(); // 実行可能なCommandを作る
             this.NextPageCommand.Subscribe(async _ => await NavigationService.NavigateAsync("SecondPage"));
         }
 
