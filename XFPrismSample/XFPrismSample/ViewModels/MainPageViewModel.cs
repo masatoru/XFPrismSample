51df439bfb19adb87d793d7b909db57f03eb4211
Storu Fujimori <satoru.fujimori@gmail.com>
2018-02-14 00:17:39 +0900

    Add command to move to SecondPage

--- XFPrismSample/XFPrismSample/ViewModels/MainPageViewModel.cs
+++ XFPrismSample/XFPrismSample/ViewModels/MainPageViewModel.cs
@@ -5,15 +5,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Reactive.Bindings;
 
 namespace XFPrismSample.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        public ReactiveCommand NextPageCommand { get; private set; }=new ReactiveCommand();
+
         public MainPageViewModel(INavigationService navigationService) 
             : base (navigationService)
         {
             Title = "Main Page";
+
+            //this.ClearCommand = this.Input
+            //    .Select(x => !string.IsNullOrWhiteSpace(x)) // Input.Valueが空じゃないとき
+            //    .ToReactiveCommand(); // 実行可能なCommandを作る
+            this.NextPageCommand.Subscribe(async _ => await NavigationService.NavigateAsync("SecondPage"));
         }
+
+
     }
 }
