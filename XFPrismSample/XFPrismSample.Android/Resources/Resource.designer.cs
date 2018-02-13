3a4a646a8de83fd174aba173fda85160cbd09317
Storu Fujimori <satoru.fujimori@gmail.com>
2018-02-14 00:16:18 +0900

    Add ReactiveProperty

--- XFPrismSample/XFPrismSample.Android/Resources/Resource.designer.cs
+++ XFPrismSample/XFPrismSample.Android/Resources/Resource.designer.cs
@@ -26,6 +26,8 @@ namespace XFPrismSample.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Reactive.Bindings.Resource.String.ApplicationName = global::XFPrismSample.Droid.Resource.String.ApplicationName;
+			global::Reactive.Bindings.Resource.String.Hello = global::XFPrismSample.Droid.Resource.String.Hello;
 			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::XFPrismSample.Droid.Resource.Attribute.actionBarSize;
 		}
 		
@@ -3624,6 +3626,12 @@ namespace XFPrismSample.Droid
 		
 		public partial class String
 		{
+			
+			// aapt resource value: 0x7f09003f
+			public const int ApplicationName = 2131296319;
+			
+			// aapt resource value: 0x7f09003e
+			public const int Hello = 2131296318;
 			
 			// aapt resource value: 0x7f090015
 			public const int abc_action_bar_home_description = 2131296277;
