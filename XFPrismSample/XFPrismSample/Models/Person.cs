cd410dac7b13153eab2fe7557bd7269bedfca011
Storu Fujimori <satoru.fujimori@gmail.com>
2018-02-14 15:32:55 +0900

    ManPageをリスト表示に修正

---
+++ XFPrismSample/XFPrismSample/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace XFPrismSample.Models
+{
+    public class Person
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "birthday")]
+        public DateTimeOffset Birthday { get; set; }
+    }
+}
--- XFPrismSample/XFPrismSample/ViewModels/MainPageViewModel.cs
+++ XFPrismSample/XFPrismSample/ViewModels/MainPageViewModel.cs
@@ -3,26 +3,31 @@ using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using Reactive.Bindings;
 using System.Reactive.Linq;
+using XFPrismSample.Models;
 
 namespace XFPrismSample.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        public ReactiveCommand NextPageCommand { get; private set; }
-        public ReactiveProperty<string> Password { get; private set; } = new ReactiveProperty<string>("");
+        public ReactiveCommand NextPageCommand { get; private set; } = new ReactiveCommand();
+        public ObservableCollection<Person> PeopleList { get; set; }
 
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
             Title = "Main Page";
 
-            this.NextPageCommand = this.Password
-                .Select(x => x == $"arrows") // 暗号が合っていれば
-                .ToReactiveCommand(); // 実行可能なCommandを作る
+            PeopleList = new ObservableCollection<Person>()
+            {
+                new Person{Id=1,Name="Ohtani"},
+                new Person{Id=2,Name="Darvish"},
+                new Person{Id=3,Name="Tanaka"},
+            };
             this.NextPageCommand.Subscribe(async _ => await NavigationService.NavigateAsync("DetailPage"));
         }
     }
