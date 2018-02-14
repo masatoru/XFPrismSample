b8a7ac6bfa71b272f591598c65ff6d1388327952
Storu Fujimori <satoru.fujimori@gmail.com>
2018-02-14 18:31:54 +0900

    PersonManagerを追加

---
+++ XFPrismSample/XFPrismSample/Models/IPersonManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XFPrismSample.Models
+{
+    public interface IPersonManager
+    {
+        ObservableCollection<Person> PeopleList { get; set; }
+    }
+}
---
+++ XFPrismSample/XFPrismSample/Models/PersonManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XFPrismSample.Models
+{
+    public class PersonManager : IPersonManager
+    {
+        public ObservableCollection<Person> PeopleList { get; set; }
+        public PersonManager()
+        {
+            PeopleList = new ObservableCollection<Person>()
+            {
+                new Person{Id=1,Name="Ohtani"},
+                new Person{Id=2,Name="Darvish"},
+                new Person{Id=3,Name="Tanaka"},
+            };
+        }
+    }
+}
